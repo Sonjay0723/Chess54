95957cc40aaea336320fbf364263e3c848129eeb
dhazari <dhrishtihazari@gmail.com>
2020-03-18 22:26:42 -0400

    TODO: add method to all pieces so that when any piece moves, it is not skipping over other pieces

--- src/Pieces/knight.java
+++ src/Pieces/knight.java
@@ -1,9 +1,32 @@
 package Pieces;
 
+import Chess.position;
+
 public class knight extends piece{
 	
 	public knight(String value) {
 		super(value);
 	}
 
+	@Override
+	public boolean validMove(position currPos, position newPos, boolean isWhite) {
+		
+		//calculate rowNumbers and column numbers
+		int rowNum = currPos.getRow();
+		int newRowNum = newPos.getRow();
+		int colNum = currPos.getColumn();
+		int newColNum = newPos.getColumn();
+		
+		//cannot be in same position
+		if(currPos.equals(newPos))
+			return false;
+		
+		//all possible moves
+		if(Math.abs(rowNum-newRowNum)==1 && Math.abs(colNum-newColNum)==2)
+			return true;
+		else if(Math.abs(rowNum-newRowNum)==2 && Math.abs(colNum-newColNum)==1)
+			return true;
+		
+		return false;
+	}
 }
--- src/Pieces/queen.java
+++ src/Pieces/queen.java
@@ -1,8 +1,36 @@
 package Pieces;
 
+import Chess.position;
+
 public class queen extends piece{
 
 	public queen(String value) {
 		super(value);
+	}
+	
+	@Override
+	public boolean validMove(position currPos, position newPos, boolean isWhite) {
+		
+		//calculate rowNumbers and column numbers
+		int rowNum = currPos.getRow();
+		int newRowNum = newPos.getRow();
+		int colNum = currPos.getColumn();
+		int newColNum = newPos.getColumn();
+		
+		//cannot be in same position
+		if(currPos.equals(newPos))
+			return false;
+		
+		//if vertical movement
+		if(Math.abs(rowNum-newRowNum)!=0 && Math.abs(colNum-newColNum)==0)
+			return true;
+		//if horizontal movement
+		else if(Math.abs(rowNum-newRowNum)==0 && Math.abs(colNum-newColNum)!=0)
+			return true;
+		//if diagonal movement
+		else if(Math.abs(rowNum-newRowNum) == Math.abs(colNum-newColNum))
+			return true;
+		
+		return false;
 	}
 }
